173409197d92b8279cb736a22ccafb5fdd99c0d9
wangslei <15701681524@163.com>
2018-10-30 14:46:06 +0800

    '1030'

---
+++ tests/Unit/UserTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+
+    }
+    public function testBasicExample()
+    {
+        $this->visit('/')
+            ->see('Laravel学院');
+    }
+}
